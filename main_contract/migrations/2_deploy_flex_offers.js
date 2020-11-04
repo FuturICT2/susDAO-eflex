60366c49efcff82e810868511341cf5fd41e336b
Leong Hei Kern <heikern@hotmail.com>
2020-11-04 18:33:56 +0100

    first runnable code in main branch

---
+++ main_contract/migrations/2_deploy_flex_offers.js
@@ -0,0 +1,5 @@
+var flex_offer = artifacts.require("Flex_Offer");
+
+module.exports = function(deployer){
+    deployer.deploy(flex_offer);
+};
--- main_contract/truffle-config.js
+++ main_contract/truffle-config.js
@@ -47,6 +47,11 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -83,6 +88,7 @@ module.exports = {
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
